3904a829a12211b60481ccbc02971679dd2ccb6c
gzuber <grzegorz.zuber@cern.ch>
2017-03-01 13:43:31 +0100

    Creating services skeletons

---
+++ src/EmailService.java
@@ -0,0 +1,5 @@
+/**
+ * Created by zuber on 01.03.17.
+ */
+public class EmailService {
+}
--- src/LhcAnomaliesDetector.java
+++ src/LhcAnomaliesDetector.java
@@ -2,5 +2,12 @@
  * Created by zuber on 01.03.17.
  */
 public class LhcAnomaliesDetector {
+  private SmsService smsService;
+  private EmailService emailService;
+  private MobileAppService mobileAppService;
+
+  public void informAboutAnomaly(String message) {
+
+  }
 
 }
